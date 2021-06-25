8ad65c301f5d0284391a0f79f7ea062ef308f0c5
thanawat-bcr <thanawat.bcr@gmail.com>
2021-06-25 17:39:21 +0700

    [SETUP]

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import Vue from 'vue';
-
+import VueCompositionAPI from '@vue/composition-api';
 import CommonButton from '@/components/CommonButton.vue';
-
 import './assets/tailwind.css';
-
 import App from './App.vue';
 import router from './router';
 
+Vue.use(VueCompositionAPI);
 Vue.component('CommonButton', CommonButton);
 
 Vue.config.productionTip = false;
