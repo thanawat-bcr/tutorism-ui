f7f909a4c9b1b445b6ed5a4780d87309e1c8228c
thanawat-bcr <thanawat.bcr@gmail.com>
2021-06-28 15:18:37 +0700

    [ADD] common image

---
+++ src/components/CommonImage.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+.common-image.w-full.h-full.overflow-hidden.relative
+  img.w-full.h-full.absolute.inset-0(
+    :class="classes"
+    :src="src"
+  )
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from '@vue/composition-api';
+
+const CommonImage = defineComponent({
+  setup(props) {
+    const objectFit = computed(() => (props.contain ? 'object-contain' : 'object-cover'));
+
+    const classes = computed(() => `${objectFit.value}`);
+
+    return {
+      classes,
+    };
+  },
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    alt: {
+      default: '',
+    },
+    contain: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+});
+
+export default CommonImage;
+</script>
+
+<style lang="scss">
+// .common-image {}
+</style>
