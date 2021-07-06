97ae2d3316e6aa1378be6107b243c45f69874f32
thanawat-bcr <thanawat.bcr@gmail.com>
2021-07-06 16:47:08 +0700

    [REMOVE] CommonCard

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
 import CommonButton from '@/components/CommonButton.vue';
-import CommonCard from '@/components/CommonCard.vue';
 import CommonImage from '@/components/CommonImage.vue';
 import CommonIcon from '@/components/CommonIcon.vue';
 
-import './assets/tailwind.css';
 import App from './App.vue';
 import router from './router';
 
 Vue.use(VueCompositionAPI);
+
 Vue.component('CommonButton', CommonButton);
-Vue.component('CommonCard', CommonCard);
 Vue.component('CommonImage', CommonImage);
 Vue.component('CommonIcon', CommonIcon);
 
