bdb283335458f5b63da91cfd73e81cc9dd5bb515
thanawat-bcr <thanawat.bcr@gmail.com>
2021-07-09 13:00:43 +0700

    [ADD] Vee Validate

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
 
+import { extend, localize } from 'vee-validate';
+import * as rules from 'vee-validate/dist/rules';
+import th from '@/vee-validate-th';
+
 import CommonButton from '@/components/CommonButton.vue';
 import CommonFloatButton from '@/components/CommonFloatButton.vue';
 import CommonImage from '@/components/CommonImage.vue';
@@ -9,9 +13,16 @@ import CommonChip from '@/components/CommonChip.vue';
 import CommonCard from '@/components/CommonCard.vue';
 import CommonPanel from '@/components/CommonPanel.vue';
 import CommonModal from '@/components/CommonModal.vue';
+import CommonForm from '@/components/CommonForm.vue';
+import CommonInput from '@/components/CommonInput.vue';
 
 import App from './App.vue';
 import router from './router';
+
+localize('th', th);
+Object.keys(rules).forEach((rule) => {
+  extend(rule, (rules as {[key: string]: any})[rule]);
+});
 
 Vue.use(VueCompositionAPI);
 
@@ -23,6 +34,8 @@ Vue.component('CommonChip', CommonChip);
 Vue.component('CommonCard', CommonCard);
 Vue.component('CommonPanel', CommonPanel);
 Vue.component('CommonModal', CommonModal);
+Vue.component('CommonForm', CommonForm);
+Vue.component('CommonInput', CommonInput);
 
 Vue.config.productionTip = false;
 
